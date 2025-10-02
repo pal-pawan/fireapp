4e599482bc34fa8c236491ba3092fbf4bd3e9532
PAWAN <pawanpal2001@gmail.com>
2025-10-02 12:41:12 +0530

    style : defined colors

---
+++ ui/Styles.ts
@@ -0,0 +1,20 @@
+const Colors = {
+    primary: "#D63B5A",
+    background: "#F8D9E1",
+    secondary: "#F2A6B3",
+    highlight: "#EB5D6A",
+    text_primary: "#D63B5A",
+    text_secondary: "#C2C2C2",
+    accent_primary : "#B2A9F6",
+    accent_secondary : "#F7C548",
+    support_blue : "#3F7BB6",
+    support_green : "#5A9B64",
+    support_offwhite : "#FFF8F3",
+}
+
+const TextStyles = {
+    text_primary_regular_20: {
+        fontSize: 20,
+        color: Colors.text_primary,
+    }
+}
